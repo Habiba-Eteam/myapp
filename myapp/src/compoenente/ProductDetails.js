5249db06813687e9a5865ee1302de320f4b1a03a
Habiba <154703093+Habiba-Eteam@users.noreply.github.com>
2024-05-20 04:04:53 +0300

    Update ProductDetails.js

--- myapp/src/compoenente/ProductDetails.js
+++ myapp/src/compoenente/ProductDetails.js
@@ -4,12 +4,12 @@ import { useParams } from "react-router-dom";
 import Product from "./Product";
 
 function ProductDetails() {
-  const habiba = "https://fakestoreapi.com/products"; // Changed variable name to habiba
+  const habiba = "https://fakestoreapi.com/products"; 
   const [product, setProduct] = useState({});
   const params = useParams();
   console.log(params);
   useEffect(() => {
-    fetch(`${habiba}/${params.productId}`) // Updated variable reference to habiba
+    fetch(`${habiba}/${params.productId}`)
       .then((res) => res.json())
       .then((product) => setProduct(product));
   }, []);
